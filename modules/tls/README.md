83aa45574c497688217ae619ee3a03c3ee68a94b
Lanqing Huang <lqhuang@outlook.com>
2026-07-10 12:46:01 +0800

    refactor: Deprecate `tls` submodule

--- modules/tls/README.md
+++
@@ -1,3 +0,0 @@
-## References
-
-- https://www.ssl.com/guide/tls-standards-compliance/
--- modules/tls/src/tls/params/KeyExchangeMode.scala
+++
@@ -1,5 +0,0 @@
-package snhttp.experimental.tls.params
-
-enum KeyExchangeMode(val code: Short, val name: String):
-  case PSK_KE extends KeyExchangeMode(0, "psk_ke")
-  case PSK_DHE_KE extends KeyExchangeMode(1, "psk_dhe_ke")
